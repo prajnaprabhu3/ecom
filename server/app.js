46142953c52f329ef67ec69f8287f3028f5c569e
Prajna <prajnapprabhu3@gmail.com>
2023-05-21 21:32:15 +0530

    deploy: cors fix

--- server/app.js
+++ server/app.js
@@ -1,39 +1,36 @@
-const express=require("express")
-const app=express();
+const express = require("express");
+const app = express();
 
-const errorMiddleware=require("./middleware/error")
+const cors = require("cors");
+app.use(cors());
 
-// installing cookie-parser used in auth.js 
-const cookieParser=require("cookie-parser");
+const errorMiddleware = require("./middleware/error");
 
-// cloudinary 
-const bodyParser=require("body-parser");
-const fileUpload=require("express-fileupload")
+// installing cookie-parser used in auth.js
+const cookieParser = require("cookie-parser");
 
-app.use(express.json())
+// cloudinary
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
 
-app.use(cookieParser()); 
+app.use(express.json());
+
+app.use(cookieParser());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
 
-
-// Route Imports 
+// Route Imports
 // product route
-const product=require("./routes/productRoute")
+const product = require("./routes/productRoute");
 
 // user route
-const user=require("./routes/userRoute")
+const user = require("./routes/userRoute");
 
+app.use("/api/v1", product);
+app.use("/api/v1", user);
 
-
-app.use("/api/v1",product);
-app.use("/api/v1",user);
-
-
-
-// Middleware for Errors 
+// Middleware for Errors
 app.use(errorMiddleware);
 
-
-module.exports=app;
+module.exports = app;
